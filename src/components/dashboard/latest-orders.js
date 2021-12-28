509e56e8aa76555c2fc537f7d079a25f1802190d
Jeebachh <yjeebachhpg@gmail.com>
2021-12-28 12:20:24 +0530

    initial commit

---
+++ src/components/dashboard/latest-orders.js
@@ -0,0 +1,153 @@
+import { format } from 'date-fns';
+import { v4 as uuid } from 'uuid';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Tooltip
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { SeverityPill } from '../severity-pill';
+
+const orders = [
+  {
+    id: uuid(),
+    ref: 'CDD1049',
+    amount: 30.5,
+    customer: {
+      name: 'Ekaterina Tankova'
+    },
+    createdAt: 1555016400000,
+    status: 'pending'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1048',
+    amount: 25.1,
+    customer: {
+      name: 'Cao Yu'
+    },
+    createdAt: 1555016400000,
+    status: 'delivered'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1047',
+    amount: 10.99,
+    customer: {
+      name: 'Alexa Richardson'
+    },
+    createdAt: 1554930000000,
+    status: 'refunded'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1046',
+    amount: 96.43,
+    customer: {
+      name: 'Anje Keizer'
+    },
+    createdAt: 1554757200000,
+    status: 'pending'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1045',
+    amount: 32.54,
+    customer: {
+      name: 'Clarke Gillebert'
+    },
+    createdAt: 1554670800000,
+    status: 'delivered'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1044',
+    amount: 16.76,
+    customer: {
+      name: 'Adam Denisov'
+    },
+    createdAt: 1554670800000,
+    status: 'delivered'
+  }
+];
+
+export const LatestOrders = (props) => (
+  <Card {...props}>
+    <CardHeader title=" Agent details" />
+    <PerfectScrollbar>
+      <Box sx={{ minWidth: 800 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Agent nane
+              </TableCell>
+              <TableCell>
+                Agent code
+              </TableCell>
+              <TableCell sortDirection="desc">
+                <Tooltip
+                  enterDelay={300}
+                  title="Sort"
+                >
+                  <TableSortLabel
+                    active
+                    direction="desc"
+                  >
+                    langauge knowing
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+              <TableCell>
+                total leads
+              </TableCell>
+              <TableCell>
+                calls
+              </TableCell>
+              <TableCell>
+               follow up
+              </TableCell>
+              <TableCell>
+                reamaining
+              </TableCell>
+              <TableCell>
+               retry call
+              </TableCell>
+              <TableCell>
+                sales
+              </TableCell>
+              <TableCell>
+               stutus
+              </TableCell>
+              <TableCell>
+                details
+              </TableCell>
+              <TableCell>
+                upload
+              </TableCell>
+            </TableRow>
+          </TableHead>
+         
+        </Table>
+      </Box>
+    </PerfectScrollbar>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        p: 2
+      }}
+    >
+     
+    </Box>
+  </Card>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,106 @@
+import Head from 'next/head';
+import { Box, Container, Grid } from '@mui/material';
+import { Budget } from '../components/dashboard/budget';
+import { LatestOrders } from '../components/dashboard/latest-orders';
+import { LatestProducts } from '../components/dashboard/latest-products';
+import { Sales } from '../components/dashboard/sales';
+import { TasksProgress } from '../components/dashboard/tasks-progress';
+import { TotalCustomers } from '../components/dashboard/total-customers';
+import { TotalProfit } from '../components/dashboard/total-profit';
+import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
+import { DashboardLayout } from '../components/dashboard-layout';
+import SearchBar from 'src/components/dashboard/search';
+import Show from 'src/components/dashboard/show';
+
+const Dashboard = () => (
+  <>
+    <Head>
+      <title>
+        Dashboard | Agent
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          
+          <Grid
+            item
+            xl={12}
+            lg={12}
+            sm={12}
+            xs={12}
+           
+          >
+            <SearchBar  />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalProfit sx={{ height: '100%'  }} />
+          </Grid>
+         
+          
+          
+          <Grid
+            item
+            lg={12}
+            md={12}
+            xl={12}
+            xs={12}
+          >
+            <LatestOrders />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
+
+Dashboard.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Dashboard;
