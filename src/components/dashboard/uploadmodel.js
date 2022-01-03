4516100aaa7932e156009230b9b28cb4e199d5e4
jeebachh <93519122+jeebachh@users.noreply.github.com>
2022-01-03 13:12:36 +0530

    Update uploadmodel.js

--- src/components/dashboard/uploadmodel.js
+++ src/components/dashboard/uploadmodel.js
@@ -1,52 +1,40 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Form,FormGroup,Input } from 'reactstrap';
-class ModalExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
 
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Button color="danger" onClick={this.toggle}>Upload</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          
-          <ModalBody>
-          <div>
-      <Card>
+const ModalExample =()=> {
+  return(
+    <div>
+      
+      <Popup  contentStyle={{ width: "500px" ,height:"180px"}} trigger={<button> Upload </button>} 
+       position="left">
+        <div><Card >
        
         <CardBody style={{textAlign:"center"}}>
           <CardTitle >Upload Excel Sheet (POPUP)	</CardTitle>
           <Form>
           <FormGroup >  
           
-          <Input style={{marginTop:"20px"}} type="file" name="file"  />
+          <Input style={{marginTop:"20px" }} type="file" name="file"  />
           
         </FormGroup>
           </Form>
-        </CardBody>
-      </Card>
-    </div>
+          <div style={{alignContent:"center"}}>
 
-          </ModalBody>
-          
-        </Modal>
-      </div>
-    );
-  }
+            <Button type='submit'>
+              submit
+            </Button>
+          </div>
+        </CardBody>
+      </Card></div>
+        
+      </Popup>
+    </div>
+    )
+
 }
 
 export default ModalExample;
